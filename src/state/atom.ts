869b5d6f76d186e29ef4f2bb9fee1c54032247eb
Vinicios Neves <vinny@macbook.lan>
2023-07-27 12:36:00 +0100

    removendo seletor async

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,11 +1,10 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
 import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos";
-import { eventosAsync } from "./seletores";
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: 'listaDeEventosState',
-  default: eventosAsync
+  default: []
 })
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
